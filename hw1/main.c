7f544c2689b3c8f9284ac1dc096bd89db829822b
lms025187 <lms025187@gmail.com>
2022-03-08 00:38:06 +0800

    6numbersto7numbers

--- hw1/main.c
+++ hw1/main.c
@@ -7,11 +7,11 @@ void print_vorform(int i) {
     fp = fopen("lotto.txt", "a+");
     time_t curtime;
     time(&curtime);
-    fprintf(fp, "======== lotto649 =========\n=");
+    fprintf(fp, "======== lotto649 =========\n= ");
     fwrite(ctime(&curtime), 24, 1, fp);
     fseek(fp, 0, SEEK_SET);
     fread(ctime(&curtime), 24, 1, fp);
-    fprintf(fp," =\n");
+    fprintf(fp,"=\n");
     fclose(fp);
 }
 
@@ -39,23 +39,23 @@ int rand_num_mod11(int lotto_array) {
     return lotto_array;
 }
 
-int get_rad_num(int lotto_array[6]) {
+int get_rad_num(int lotto_array[7]) {
 
-    for(int j = 0; j < 6; j++) {
-        if(j < 5){
+    for(int j = 0; j < 7; j++) {
+        if(j < 6){
             lotto_array[j] = rand_num_mod70(lotto_array[j]);
-        } else if (j = 5) {
-            lotto_array[5] = rand_num_mod11(lotto_array[5]);
+        } else if (j = 6) {
+            lotto_array[6] = rand_num_mod11(lotto_array[6]);
         }
         //check lotto array[i] isn't 0;
     }
-    return lotto_array[6];
+    return lotto_array[7];
 }
 
-int array_swap(int lotto_array[6]) {
+int array_swap(int lotto_array[7]) {
     int tmp = 0;
-    for(int j = 0; j < 5 ;j++) {
-        for(int arr_lotto = j; arr_lotto < 5; arr_lotto++) {
+    for(int j = 0; j < 6 ;j++) {
+        for(int arr_lotto = j; arr_lotto < 6; arr_lotto++) {
             if (lotto_array[j] > lotto_array[arr_lotto]) {
                 tmp = lotto_array[j];
                 lotto_array[j] = lotto_array[arr_lotto];
@@ -63,25 +63,25 @@ int array_swap(int lotto_array[6]) {
             }
         }
     }
-    return lotto_array[6];
+    return lotto_array[7];
 }
 
-int array_rerandom(int lotto_array[6]) {
-    for (int j = 0; j < 5; j++) {
-        for(int z =0 ; z <5; z++){
+int array_rerandom(int lotto_array[7]) {
+    for (int j = 0; j < 6; j++) {
+        for(int z =0 ; z < 6; z++){
             while ((lotto_array[j] == lotto_array[z] && j != z) || 
-                    lotto_array[j] == lotto_array[5]) {
+                    lotto_array[j] == lotto_array[6]) {
                 lotto_array[j] = rand_num_mod70(lotto_array[j]);
             }
         }
     }
-    return lotto_array[6];
+    return lotto_array[7];
 }
 
-int print_array (int lotto_array[6]) {
+int print_array (int lotto_array[7]) {
     FILE* fp;
     fp = fopen("lotto.txt", "a");
-    for(int j = 0 ; j < 6 ; j++) {
+    for(int j = 0 ; j < 7 ; j++) {
         fprintf(fp, "%.2d ", lotto_array[j]);
     }
     fprintf(fp, "\n");
@@ -91,11 +91,11 @@ int print_array (int lotto_array[6]) {
 int print_last(int num_row) {
     FILE* fp;
     fp = fopen("lotto.txt", "a+");
-    for(int i = num_row+1; i <= 6; i++) {
+    for(int i = num_row+1; i < 6; i++) {
         fprintf(fp,"[%d]: " , i);
-        for(int j = 0; j < 6; j++) {
+        for(int j = 0; j < 7; j++) {
             fprintf(fp, "-- ");
-            j == 5 ? fprintf(fp,"\n") : 1;
+            j == 6 ? fprintf(fp,"\n") : 1;
         }
     }
     fprintf(fp, "======== csie@CGU =========\n");
@@ -111,11 +111,11 @@ int main() {
     print_vorform(row_num); //fprintf1
     srand((unsigned) curtime);
     for (int i = 1; i <= row_num; i++) {
-        int lotto_array[7] = {0};
+        int lotto_array[8] = {0};
         get_rad_num(lotto_array);
         fwriteforma(i); //fprintf2
-        lotto_array[6] = array_rerandom(lotto_array);
-        lotto_array[6] = array_swap(lotto_array);
+        lotto_array[8] = array_rerandom(lotto_array);
+        lotto_array[8] = array_swap(lotto_array);
         print_array(lotto_array); //fprintf3
     }
     print_last(row_num); //fprintf4
